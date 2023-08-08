12873cc9c57f4da189427d66a2aa2570ef92a9ae
pcmonosolution <pc@monosolutions.com>
2023-08-08 13:34:38 -0500

    removed useffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Header from "./components/Header/Header"
 import NavigationMenu from "./components/NavigationMenu/NavigationMenu"
 import ResourceList from "./components/ResourceList/ResourceList" // Import the ResourceList component
